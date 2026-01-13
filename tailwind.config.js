665854ebca43e9f4c019eeab080d725ebd93d97d
Zein-praft <zeinfahmi449@gmail.com>
2026-01-13 10:04:43 +0800

    add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./app/Livewire/**/*.php"
+    "./app/Livewire/**/*.php",
   ],
   theme: {
     extend: {},
