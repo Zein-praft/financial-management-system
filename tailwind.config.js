4c906cbb1acb6057613a913c474de08224fd1619
Zein-praft <zeinfahmi449@gmail.com>
2026-01-15 13:25:50 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./app/Livewire/**/*.php",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    darkMode: 'class',
+    content: [
+        './vendor/laravel/framework/src/resources/**/*.blade.php',
+        './storage/framework/views/**/*.blade.php',
+        './resources/views/**/*.blade.php',
+        './resources/views/livewire/**/*.blade.php', 
+        './resources/js/**/*.js',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
